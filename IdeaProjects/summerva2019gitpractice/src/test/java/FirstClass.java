973e1c912c1d0b45fa1dcc489675b6727bff3c35
Gulmire Yusuf <gulmi199056@gmail.com>
2019-11-21 16:30:19 -0500

    initial commit

--- IdeaProjects/summerva2019gitpractice/src/test/java/FirstClass.java
+++ IdeaProjects/summerva2019gitpractice/src/test/java/FirstClass.java
@@ -2,5 +2,7 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("I am working");
+        System.out.println("I am sleeping");
     }
 }
