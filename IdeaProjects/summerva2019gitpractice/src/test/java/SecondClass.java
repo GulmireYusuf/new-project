0e7a1387a6800e9f0ac205e25bec2ba96c0ca453
Gulmire Yusuf <gulmi199056@gmail.com>
2019-11-21 16:55:46 -0500

    initial commit

---
+++ IdeaProjects/summerva2019gitpractice/src/test/java/SecondClass.java
@@ -0,0 +1,9 @@
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
+public class SecondClass {
+
+    public static void main(String[] args) {
+        System.out.println("heloo");
+        System.out.println("git push -u origin master");
+    }
+}
